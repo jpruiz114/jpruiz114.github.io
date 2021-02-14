19b669731ece3014d6a0e68654b7906589380516
Jean Paul Ruiz <jpruiz114@gmail.com>
2021-02-14 16:34:37 -0500

    Contact form changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,6 @@
     let toggle = document.getElementById('menu-toggle');
     let menu = document.getElementById('menu');
     let close = document.getElementById('menu-close');
-    let form = document.getElementById('contact');
 
     toggle.addEventListener('click', function (e) {
         if (menu.classList.contains('open')) {
@@ -14,16 +13,6 @@
 
     close.addEventListener('click', function (e) {
         menu.classList.remove('open');
-    });
-
-    form.addEventListener('submit', function (e) {
-        if (e.preventDefault) {
-            e.preventDefault();
-        }
-
-        console.log('form submit');
-
-        return false;
     });
 
     // Close menu after click on smaller screens
